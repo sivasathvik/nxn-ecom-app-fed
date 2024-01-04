370e4f12fac8f282015d7e615d2081ec2f708803
sivasathvik <paturisivasathvik@gmail.com>
2024-01-04 11:46:49 +0530

    Used formik and yupp for validation and form handling in login page

--- fed/src/app/components/Login.tsx
+++ fed/src/app/components/Login.tsx
@@ -2,23 +2,41 @@ import { Box, Button, Container, Grid, Link, TextField, Typography } from "@mui/
 import React from "react";
 //import ProjAppBar from "./ProjAppBar";
 //import { Login } from "@mui/icons-material";
+import {useFormik} from "formik";
+import * as Yup from 'yup';
 
 const Login = () => {
+    const formik = useFormik({
+        initialValues: {
+          email: "",
+          password: "",
+        },
+        validationSchema: Yup.object({
+          email: Yup.string().email('Invalid email address').required('Email is Required'),
+          password:Yup.string().min(6,"Must be atleast 6 characters long").required("Password is required"),
+        }),
+        onSubmit: async (values) => {
+          //setTimeout( () => {alert(JSON.stringify(values, null, 2));} ,5000);
+          alert(JSON.stringify(values, null, 2));
+          //await register(values);
+        },
+      });
   return (
     <React.Fragment>
-      <Container>
+    <form onSubmit={formik.handleSubmit}>
+    <Container>
         <Grid container spacing={2} pt={2} px={25}>
           <Grid item xs={12}>
             <Typography variant="h4">Login</Typography>
           </Grid>
           <Grid item xs={12}>
-            <TextField fullWidth id="outlined-basic" label="Email" variant="outlined" />
+            <TextField fullWidth id="email" name="email" label="Email" variant="outlined" value={formik.values.email} onChange={formik.handleChange} error={Boolean(formik.errors.email)} helperText={formik.errors.email}  />
           </Grid>
           <Grid item xs={12}>
-            <TextField fullWidth id="outlined-basic" label="Password" variant="outlined" />
+            <TextField fullWidth id="password" name="password" label="Password" variant="outlined" value={formik.values.password} onChange={formik.handleChange} error={Boolean(formik.errors.password)} helperText={formik.errors.password}   />
           </Grid>
           <Grid item xs={12}>
-            <Button variant="contained" fullWidth>
+            <Button variant="contained" fullWidth type='submit' disabled={!formik.isValid || formik.isSubmitting}>
               Login
             </Button>
           </Grid>
@@ -34,6 +52,8 @@ const Login = () => {
           </Grid>
         </Grid>
       </Container>
+
+    </form>
     </React.Fragment>
   );
 };
